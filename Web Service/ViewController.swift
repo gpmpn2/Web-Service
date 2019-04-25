5e34e016cab95c0429c9c8e6eaec869117a5b373
Grant <grant.maloney@live.com>
2019-04-25 17:40:14 -0500

    initial commit

---
+++ Web Service/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Web Service
+//
+//  Created by Grant Maloney on 4/25/19.
+//  Copyright © 2019 Grant Maloney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Web ServiceTests/Web_ServiceTests.swift
@@ -0,0 +1,34 @@
+//
+//  Web_ServiceTests.swift
+//  Web ServiceTests
+//
+//  Created by Grant Maloney on 4/25/19.
+//  Copyright © 2019 Grant Maloney. All rights reserved.
+//
+
+import XCTest
+@testable import Web_Service
+
+class Web_ServiceTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
